59166f7bd5e6d94394f38ef1c4d3291acfee9b9c
Kalle Ott <kalle.ott@btcx.com>
2025-02-12 11:58:44 +0100

    endpoint implementation to create OTPs

---
+++ app/route.ts
@@ -0,0 +1,18 @@
+import { authenticator } from 'otplib'
+import { z } from 'zod'
+
+const requestSchema = z.object({
+    secret: z.string(),
+})
+
+export async function POST(request: Request) {
+    const body = await request.json()
+
+    const { secret } = requestSchema.parse(body)
+
+    const otp = authenticator.generate(secret)
+
+    return Response.json({
+        otp,
+    })
+}
